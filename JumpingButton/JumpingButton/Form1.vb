7a55f963a7fc7e8418e1306a0bdcab7b3262e749
flynn333 <flynn333@umn.edu>
2013-09-11 22:38:39 -0500

    Update Form1.vb

--- JumpingButton/JumpingButton/Form1.vb
+++ JumpingButton/JumpingButton/Form1.vb
@@ -1,6 +1,20 @@
-﻿Public Class Form1
+Public Class FrmHome
 
-    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+
+
+    Private Sub MoveExit_Click(sender As System.Object, e As System.EventArgs) Handles Btn1MoveExit.MouseEnter
+
+        Dim NumX As New Integer
+        Dim NumY As New Integer
+        NumX = Rnd() * CInt(Me.Width - Btn1MoveExit.Width - 14)
+        NumX = Math.Round(NumX)
+        NumY = Rnd() * CInt(Me.Height - Btn1MoveExit.Height - 36)
+        NumY = Math.Round(NumY)
+        Btn1MoveExit.Location = New Point(NumX, NumY)
+
+    End Sub
+
+    Private Sub FrmHome_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 End Class
