3f976781bf085ed44fda4e06b15fd16b0aa3ccfa
flynn333 <flynn333@umn.edu>
2013-09-11 22:37:21 -0500

    HW1 Flynn333

--- JumpingButton/JumpingButton/Form1.Designer.vb
+++ JumpingButton/JumpingButton/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class FrmHome
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,29 +22,35 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Btn1MoveExit = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Button1
+        'Btn1MoveExit
         '
-        Me.Button1.Location = New System.Drawing.Point(87, 164)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(121, 47)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "What's up?"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Btn1MoveExit.Location = New System.Drawing.Point(245, 238)
+        Me.Btn1MoveExit.Name = "Btn1MoveExit"
+        Me.Btn1MoveExit.Size = New System.Drawing.Size(77, 48)
+        Me.Btn1MoveExit.TabIndex = 0
+        Me.Btn1MoveExit.Text = "Click Me and Win!"
+        Me.Btn1MoveExit.UseVisualStyleBackColor = True
         '
-        'Form1
+        'FrmHome
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(292, 267)
-        Me.Controls.Add(Me.Button1)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(684, 662)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Btn1MoveExit)
+        Me.MaximumSize = New System.Drawing.Size(700, 700)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(700, 700)
+        Me.Name = "FrmHome"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Click me"
+        Me.TopMost = True
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Btn1MoveExit As System.Windows.Forms.Button
 
 End Class
